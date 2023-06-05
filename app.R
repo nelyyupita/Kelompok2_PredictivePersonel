76bc535e92ade00d757c483695c601c1c2ca3d13
indra wiryanto <indrarencongsakti33@gmail.com>
2023-06-05 17:30:54 +0700

    update

--- app.R
+++ app.R
@@ -8,31 +8,34 @@ library(DT)
 library(echarts4r) 
 
 data <- read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
-view(data)
+#view(data)
 
 left <- c("left", "stay", "","","")
 id <- c(1:1000)
 ui <- dashboardPage(
   dashboardHeader(
-    title = "SIKAP"
+    title = "HR Dashboard"
   ),
   dashboardSidebar(
     sidebarMenu(
       id = "tabs",
-      menuItem("Main Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Attrition Dashboard", tabName = "emp", icon = icon("users")),
-      menuItem("Employee Profiler", tabName = "empp", icon = icon("user")),
-      menuItem("Employee Databases", tabName = "emppp", icon = icon("users"))
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Employee Dashboard", tabName = "emp", icon = icon("user")),
+      menuItem("Profile Employe", tabName = "profile", icon = icon("person")),
+      menuItem("Employee", tabName = "empp", icon = icon("users"))
+      
     )
   ),
   dashboardBody(
     bookmarkButton(),
     tabItems(
       tabItem(tabName = "dashboard",
-              h1("Main Dashboard"),
+              h1("Ini menu Dashboard"),
               fluidRow(
-                valueBox(date(),"Date",icon = icon("calendar-days"), color = "aqua",  width = 6),
-                valueBox("1233","Existing Employees", icon = icon("users"),color = "aqua",  width = 6),
+                valueBox("2023-06-02","Date",icon = icon("calendar-days"), color = "light-blue",  width = 3),
+                valueBox("3571","Existing Employees", icon = icon("users"),color = "teal",  width = 3),
+                valueBox("169","Work Accidents",icon = icon("briefcase"), color = "orange",  width = 3),
+                valueBox("3552","Unprometed Employees", icon = icon("info"),color = "fuchsia",  width = 3),
               ),
               fluidRow(
                 column(
@@ -78,7 +81,7 @@ ui <- dashboardPage(
         h1("Employee Dashboard"),
         selectInput("emp","Select Employee by ID", id),
         actionButton("btn","Filter"), 
-        textOutput("output") ,
+        textOutput("textOutput") ,
         
         h1("Employee Data"),
         fluidRow(
@@ -98,92 +101,254 @@ ui <- dashboardPage(
         ),
         
       ),
+      #Update
+      tabItem(
+        tabName = "profile",
+        h1("Employ Profile"),
+        
+        selectInput(inputId = "profile", label = "id employee", choices = data$EmployeeNumber),
+        actionButton(inputId = "Btn", label = "search", style= "margin-bottom:14px"),
+        
+        uiOutput("coba"),
+        fluidRow(
+          valueBoxOutput("Yes", width = 3),
+          valueBoxOutput("rate", width = 3),
+          valueBoxOutput("Sales", width = 3),
+          valueBoxOutput("roles", width = 3)
+        ),
+        
+        box(width = 4,
+            h3("Personal Information"),
+            textOutput(outputId = "edu"),
+            textOutput(outputId = "age"),
+            textOutput(outputId = "gender"),
+            textOutput(outputId = "edufild")),
+        
+        box(width = 4,
+            h3("Job Related Information"),
+            textOutput(outputId = "job"),
+            textOutput(outputId = "month"),
+            textOutput(outputId = "number"),
+            textOutput(outputId = "travel")),
+        
+        box(width = 4,
+            h3("Job Satisfaction Information"),
+            textOutput(outputId = "satis"),
+            textOutput(outputId = "jobsatis"),
+            textOutput(outputId = "relation"),
+            textOutput(outputId = "work")
+        )
+        
+        
+      ),
       
       tabItem(
         tabName = "empp",
         h1("Employee List"),
         dataTableOutput("dynamic"),
-      ),
-      
-      tabItem(
-        tabName = "emppp",
-        h1("Employee List"),
-        dataTableOutput("dynamic"),
       )
-      
-    ),
+    )
   )
 )
 
+
 server <- function(input, output) {
+  
+  ###########################################################################################
+  filterData <- eventReactive(input$Btn,{
+    data %>% filter(EmployeeNumber == input$profile)
+  })
+  
+  
+  output$roles <- renderValueBox({
+    req(filterData())
+    data <-  filterData()%>%
+      select(JobRole)
+    
+    valueBox(value= data , subtitle = "Employee Roles", icon = icon("briefcase"))
+  })
+  
+  #######################################
+  filterData <- eventReactive(input$Btn,{
+    data %>% filter(EmployeeNumber == input$profile)
+  })
+  
+  
+  output$Yes <- renderValueBox({
+    req(filterData())
+    data <-  filterData()%>%
+      select(OverTime)
+    
+    valueBox(value= data$OverTime , subtitle = "Attantion status", icon = icon("user-times"), color = "red")
+  })
+  
+  ########################################################################
+  
+  filterData <- eventReactive(input$Btn,{
+    data %>% filter(EmployeeNumber == input$profile)
+  })
+  
+  
+  output$rate <- renderValueBox({
+    req(filterData())
+    data <-  filterData()%>%
+      select(PerformanceRating)
+    
+    valueBox(value= data$PerformanceRating , subtitle = "Performance Rating", icon = icon("star"))
+  })
+  ##################################################
+  
+  filterData <- eventReactive(input$Btn,{
+    data %>% filter(EmployeeNumber == input$profile)
+  })
+  
+  
+  output$Sales <- renderValueBox({
+    req(filterData())
+    data <-  filterData()%>%
+      select(Department)
+    
+    valueBox(value= data$Department , subtitle = "Department", icon = icon("user"))
+  })
+  
+  
+  
+  
+  output$edu <- renderText({
+    paste0("Education Level: Level ", filterData()$Education)
+  })
+  
+  output$age <- renderText({
+    paste0("Age: ", filterData()$Age)
+  })
+  
+  output$gender <- renderText({
+    paste0("Gender: ", filterData()$Gender)
+  })
+  
+  output$edufild <- renderText({
+    paste0("Education Field ", filterData()$EducationField)
+  })
+  
+  output$job <- renderText({
+    paste0("Job :", filterData()$JobRole)
+  })
+  
+  output$month <- renderText({
+    paste0("Monthly Income : ", filterData()$MonthlyIncome)
+  })
+  
+  output$number <- renderText({
+    paste0("Number of Companies Worked:  ", filterData()$NumCompaniesWorked)
+  })
+  
+  output$travel <- renderText({
+    paste0("Business Travel Frequency : ", filterData()$BusinessTravel)
+  })
+  
+  
+  output$satis <- renderText({
+    paste0("Environment Satisfaction: ", filterData()$EnvironmentSatisfaction)
+  })
+  
+  
+  output$jobsatis <- renderText({
+    paste0("Job Satisfaction: ", filterData()$JobSatisfaction)
+  })
+  
+  output$relation <- renderText({
+    paste0("Relationship Satisfaction: ", filterData()$RelationshipSatisfaction)
+  })
+  
+  output$work <- renderText({
+    paste0("Work Life Balance: ", filterData()$WorkLifeBalance)
+  })
+  
+  ###########################################################################################
+  
+  
+  
+  
+  
+  
+  
+  output$coba <- renderUI({
+    req(filterData())
+    tagList(
+      h1("Employe Data"))
+    
+  }
+  )
+  
+  
   
   
   set.seed(122)
-  histdata <- rnorm(500)
+  # histdata <- rnorm(500)
+  # 
+  # output$plot1 <- renderPlot({
+  #   data <- histdata[seq_len(input$slider)]
+  #   hist(data)
+  # })
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
+  # output$visualisasi1 <- renderEcharts4r({
+  #   data %>%
+  #     #filter(left == 0) %>%
+  #     #gunakan kolom sales untuk mengganti _
+  #     count(JobRole) %>%
+  #     arrange(n) %>%
+  #     e_charts(JobRole) %>%
+  #     e_bar(n, name = "Role") %>%
+  #     e_tooltip() %>%
+  #     e_legend() %>%
+  #     e_flip_coords()%>%
+  #     e_color(color = "teal", background = "white")  
+  # })
+  # 
+  # output$visualisasi2 <- renderEcharts4r({
+  #   data %>%
+  #     count(salary) %>%
+  #     e_charts(salary) %>%
+  #     e_pie(n, name = "salary") %>%
+  #     e_tooltip() %>%
+  #     e_legend()
+  #   
+  # })
   
-  output$visualisasi1 <- renderEcharts4r({
-    data %>%
-      #filter(left == 0) %>%
-      #gunakan kolom sales untuk mengganti _
-      count(sales) %>%
-      arrange(n) %>%
-      e_charts(sales) %>%
-      e_bar(n, name = "sales") %>%
-      e_tooltip() %>%
-      e_legend() %>%
-      e_flip_coords()%>%
-      e_color(color = "teal", background = "white")  
-  })
+  # output$visualisasi3 <- renderEcharts4r({
+  #   data %>%
+  #     count(last_evaluation) %>%
+  #     arrange(n) %>%
+  #     e_charts(last_evaluation) %>%
+  #     #e_charts(satisfaction_level) %>%
+  #     e_bar(n, name = "") %>%
+  #     e_tooltip() %>%
+  #     e_flip_coords() %>%
+  #     e_legend()
+  # })
   
-  output$visualisasi2 <- renderEcharts4r({
-    data %>%
-      count(salary) %>%
-      e_charts(salary) %>%
-      e_pie(n, name = "salary") %>%
-      e_tooltip() %>%
-      e_legend()
-    
-  })
+  # output$visualisasi4 <- renderEcharts4r({
+  #   data %>%
+  #     count(sales) %>%
+  #     arrange(n) %>%
+  #     e_charts(sales) %>%
+  #     e_bar(n, name = "sales") %>%
+  #     e_tooltip() %>%
+  #     e_legend() %>%
+  #     e_flip_coords() %>%
+  #     e_color(color = "teal", background = "white")  
+  # })
   
-  output$visualisasi3 <- renderEcharts4r({
-    data %>%
-      count(last_evaluation) %>%
-      arrange(n) %>%
-      e_charts(last_evaluation) %>%
-      #e_charts(satisfaction_level) %>%
-      e_bar(n, name = "") %>%
-      e_tooltip() %>%
-      e_flip_coords() %>%
-      e_legend()
-  })
-  
-  output$visualisasi4 <- renderEcharts4r({
-    data %>%
-      count(sales) %>%
-      arrange(n) %>%
-      e_charts(sales) %>%
-      e_bar(n, name = "sales") %>%
-      e_tooltip() %>%
-      e_legend() %>%
-      e_flip_coords() %>%
-      e_color(color = "teal", background = "white")  
-  })
-  
-  output$visualisasi5 <- renderEcharts4r({
-    data %>%
-      count(sales) %>%
-      arrange(n) %>%
-      e_charts(sales) %>%
-      e_bar(n, name = "sales") %>%
-      e_tooltip() %>%
-      e_legend()
-    
-  })
+  # output$visualisasi5 <- renderEcharts4r({
+  #   data %>%
+  #     count(sales) %>%
+  #     arrange(n) %>%
+  #     e_charts(sales) %>%
+  #     e_bar(n, name = "sales") %>%
+  #     e_tooltip() %>%
+  #     e_legend()
+  #   
+  # })
   
   ev <- eventReactive(input$btn,{
     input$emp
